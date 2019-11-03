7a745e6ab63345a74c805b2de250e7263a33405d
inherited-window-0818 <robt15@hotmail.com>
2019-11-03 23:05:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,7 +10,7 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   monopoly = {
-    :railroads => {}
+    :railroads=> {}
   }
 end
 
@@ -20,10 +20,10 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
    monopoly = {
-    :railroads => {
-      :pieces => 4,
-      :rent_in_dollars => {},
-      :names => {}
+    :railroads=> {
+      :pieces=> 4,
+      :rent_in_dollars=> {},
+      :names=> {}
     }
   }
 end
