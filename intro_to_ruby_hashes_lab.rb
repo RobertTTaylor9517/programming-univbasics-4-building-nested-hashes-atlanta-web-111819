2455dcfc9ec6e7999619006be99495d8351c4ae7
inherited-window-0818 <robt15@hotmail.com>
2019-11-03 23:13:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,10 +35,10 @@ def monopoly_with_third_tier
         :four_pieces_owned => 200
       },
       :names => {
-        :reading_railroad=> {"mortgage value" => "100" },
-        :pennsylvania_railroad=> {"mortgage value" => "200"},
-        :b_and_o_railroad=> {"mortgage value" => "400"},
-        :shortline=> {"mortgage value" => "800"}
+        :reading_railroad=> {},
+        :pennsylvania_railroad=> {},
+        :b_and_o_railroad=> {},
+        :shortline=> {}
       }
     }
   }
